a4af60b5bc72c1584d52cabf0d1858a29018ba15
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-03-17 21:23:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,7 @@ end
 
   def assign_rooms(attendees)
     attendees.each_with_index.map do |speaker, index|
-    "Hello, #{speaker}! You'll be assigned to room #{index
-    +1}!"
+    "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
   end
 end
 
