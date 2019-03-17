fb97791a5eea99be8b8c8045f9624e8a12302a29
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-03-17 21:06:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+attendees = [Edsger, Ada, Alan, Grace, Linus, Matz]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+  
+  def batch_badge_creator(attendees)
+    messages = []
+    attendees.each do |badges|
+    "Hello, my name is #{badges}."
+  end
+  messages
+end
+
+  def assign_rooms(speakers)
+    speakers.each_with_index.map do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index
+    +1}!"
+  end
+end
+
+def printer()
+
+
+
+  
